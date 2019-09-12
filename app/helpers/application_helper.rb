b1cf1bf3fa7e678af04a4dba4d96b3b71009e796
ShogoKinjo <shogo14kinjo@gmail.com>
2019-09-12 16:52:04 +0900

    Add Guard Gems,Update BootStrap Ver

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "News Croller"
+    base_title = "News Crawler"
     if page_title.empty?
       base_title
     else
