50ca6cc3214f17f44140ead34a058036ab1ba399
ShogoKinjo <shogo14kinjo@gmail.com>
2019-09-23 14:13:46 +0900

    User advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_130542) do
+ActiveRecord::Schema.define(version: 2019_09_19_070138) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_14_130542) do
     t.boolean "delete_flg"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
